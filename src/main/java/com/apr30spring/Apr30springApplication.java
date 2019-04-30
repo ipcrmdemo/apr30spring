b247f978fecbdcd3e0f1a2e77282feb89f94c8ee
Matt Cadorette <mcadorette@gmail.com>
2019-04-30 12:39:49 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/apr30spring/Apr30springApplication.java
@@ -0,0 +1,12 @@
+package com.apr30spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apr30springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apr30springApplication.class, args);
+	}
+}
